ca484ba2a348f0fb04a2e66fbbf4e35a33451eec
xiedong <xiedong11@aliyun.com>
2019-02-13 18:35:02 +0800

    资源列表页接口数据对接

--- src/api/api.js
+++ src/api/api.js
@@ -1,28 +1,42 @@
-import { url, get, post } from "edu-common"
+import {url, get, post} from "utils/axios/ApiDecorator"
 
 class Api {
-	// 测试 get接口
-	@url("/exercise/userMonthStatis")
-	@get
-    getTest() {}
-	
-	// 获取token
-	// 测试 post接口
-	@url("/we-base-sso-provider/sso/login")
-	@post
-	login() {}
+    // 测试 get接口
+    @url("/exercise/userMonthStatis")
+    @get
+    getTest() {
+    }
 
-	// 提交json数组使用postArray装饰
-	@url('/exercise/subAnswer')
-	@post
-	subAnswer() { }
+    // 获取token
+    // 测试 post接口
+    @url("/we-base-sso-provider/sso/login")
+    @post
+    login() {
+    }
 
-	
-	// 获取用户详情
-	@url('/wisdom/user/detail')
-	@get
-	getUserInfo() {}
-		
+    // 提交json数组使用postArray装饰
+    @url('/exercise/subAnswer')
+    @post
+    subAnswer() {
+    }
+
+
+    // 获取用户详情
+    @url('/wisdom/user/detail')
+    @get
+    getUserInfo() {
+    }
+
+    /**
+     * 获取课程下的视频和测试
+     *
+     * @Title: getContentByCurrId
+     */
+    @url('/zhlInterfaceUnifyEntry')
+    @post
+    getContentByCurrId() {
+    }
+
 }
 
 export default new Api()
--- src/pages/lecture/lectureList.jsx
+++ src/pages/lecture/lectureList.jsx
@@ -1,22 +1,14 @@
 import React, {Component} from "react"
 import './less/lectureList.less'
+import Api from '../../api/api'
+import DoApi from "../../utils/axios/DoApi";
 
 class LectureList extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            lectureList: [
-                {
-                    title: "几何图形初步学习"
-                },
-                {
-                    title: "打发第三方的撒发射点"
-                },
-                {
-                    title: "发生大范围发生大范围"
-                }
-            ]
+            result: {}
         }
     }
 
@@ -24,21 +16,49 @@ class LectureList extends Component {
         util.goForward(`/lectureDetail`, this)
     }
 
+    componentDidMount() {
+        this.getContentByCurrId()
+    }
+
+
+    getContentByCurrId = async () => {
+        let className = "com.zhl.unify.interfaces.move_work.service.ZwxClientService";
+        let method = "getContentByCurrId";
+
+        var contentJson = {
+            "currId": 491,
+            "token": "9F5336D314F887F46834B0C6A0EE4C7064FC8880E17A43508C8A9A8B93B1495A801A35BF2CD1F1B452FDB19EC0FD61DF"
+        }
+        let params = new DoApi.createParamJSON(className, method, contentJson)
+        let result = await Api.getContentByCurrId(params)
+
+
+        this.setState({
+                result
+            }
+        )
+    }
+
+
     render() {
-        let {lectureList} = this.state
+        let {result} = this.state
+        console.log("111", result, result.data)
         return (
             <div id='root'>
                 <div id='title-bar'>
                     <span id='title'>几何图形课程列表</span>
                 </div>
                 {
-                    (lectureList || []).map((item, index) => {
+                    (result.data && result.data.content.result.res || []).map((item, index) => {
                             return (
                                 <div key={index} id='item-root' onClick={this.onClickLectureItem}>
-                                    <img id='imgs' src='https://avatar.csdn.net/6/0/6/3_xieluoxixi.jpg'/>
+                                    <div id ='img-root'>
+                                        <img id='imgs' src='https://avatar.csdn.net/6/0/6/3_xieluoxixi.jpg'/>
+                                        <span id = 'text-time'>{item.videoDuration}</span>
+                                    </div>
                                     <div id='text-container'>
-                                        <span id='title-text'>{item.title}</span>
-                                        <span id='desc-text'>集合图像的 总奖金二九九二九二</span>
+                                        <span id='title-text'>{item.videoTitle}</span>
+                                        <span id='desc-text'>{item.videoDesc}</span>
                                     </div>
                                 </div>
                             )
