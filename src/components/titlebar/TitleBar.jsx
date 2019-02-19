0bb3a64cc11e9fbf542f6cbe4698791587e915ab
xiedong <xiedong11@aliyun.com>
2019-02-19 09:14:10 +0800

    公用组件TitleBar添加右侧文字功能绑定回调事件

--- src/components/titlebar/TitleBar.jsx
+++ src/components/titlebar/TitleBar.jsx
@@ -15,8 +15,9 @@ class TitleBar extends Component {
         return (
             <div id='title-bar'>
                 <img id='img-back' src='../../../public/static/img/ic_back.png' onClick={this.onBackClick}></img>
-                <span id='title' >{this.props.title}</span>
+                <span id='title'>{this.props.title}</span>
                 <img id='right-text' onClick={this.props.onRightClick} src={this.props.rightImgPath}/>
+                <span id='right-text' onClick={this.props.onRightClick}>{this.props.rightText}</span>
             </div>
         )
     }
