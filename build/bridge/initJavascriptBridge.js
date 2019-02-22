1258fe9500f53604c88064d9136a5cb850ded744
xiedong <xiedong11@aliyun.com>
2019-02-22 10:58:11 +0800

    deploy

---
+++ build/bridge/initJavascriptBridge.js
@@ -0,0 +1,21 @@
+//注册事件监听，初始化
+function connectWebViewJavascriptBridge(callback) {
+    console.log(100)
+    if (window.WebViewJavascriptBridge) {
+        callback(WebViewJavascriptBridge)
+    } else {
+        document.addEventListener(
+            'WebViewJavascriptBridgeReady'
+            , function() {
+                callback(WebViewJavascriptBridge)
+            },
+            false
+        );
+    }
+}
+
+//回调函数，接收java发送来的数据
+connectWebViewJavascriptBridge(function(bridge) {
+    bridge.init(function(message, responseCallback) {
+    });
+})
